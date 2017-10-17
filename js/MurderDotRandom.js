2253006b0ec1a0860821f36ad2c6e64a465603d9
Elvin <ttop34@icloud.com>
2017-10-17 18:03:59 -0500

    made a bunch of stuff.

---
+++ js/MurderDotRandom.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+// all guest names are just place holders//
+var guests = ["Jack","Jill","Pinky","Brain", "Barack", "Vladimir", "Shaggy", "Morty", "Morgan", "Freeman"];
+
+//10 hometowns//
+var homeTown = [""];
+
+var gender = ["M","F"];
+
+// 5 professions//
+var profession = [];
+
+var strikes= 3;
+
+var clues = [];
+
+var innocent = [];
+
+var rooms = ["living room", "dinning"]
+
+function createGame(){
+
+}
+
+
+function guilty(){
+
+}
+
+
+function accuse(accused) {
+}
+
+
