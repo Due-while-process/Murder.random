64720715c5f7cb3433f167aaf4a0d848d221bf9b
ironicsushi <yuli5565@colorado.edu>
2017-11-05 10:25:59 -0600

    * refactored into four separate files instead of just one

---
+++ js/clues.js
@@ -0,0 +1,225 @@
+////////////////////////////////
+//                            //
+//          CLUES             //
+//                            //
+////////////////////////////////
+
+
+var clueOne = {
+    type: "briefcase",
+    description: "a briefcase full of papers",
+    includes: {
+        gender: ["F"],
+        hometown: ["Orlando","Austin"]
+    },
+    excludes: {
+        age: "young adult"
+    }
+};
+var clueTwo = {
+    type: "hair",
+    description: "a single strand of white hair",
+    include: {
+        age: ["adult", "senior"]
+    },
+    exclude: {
+        age: ["teenager", "young adult"]
+    }
+};
+var clueThree = {
+    type: "cleaning supply",
+    description: "a rare cleaning agent consisting of dihydrogen monoxide",
+    include: {
+        profession: ["Teacher", "Engineer"],
+        hometown: ["NYC", "LA", "San Antonio"]
+    },
+    exclude: {
+        age: ["teenager"]
+    }
+};
+var clueFour = {
+    type: 'murder weapon',
+    description: 'cane',
+    include: {
+        gender: ['F'],
+        hometown: ['Denver', 'San Antonio']
+    },
+    exclude: {
+        age: ['young adult']
+    }
+};
+var clueFive = {
+    type: 'text on phone',
+    description: '(512) 555-1234 - We still on for dinner tomorrow night?',
+    include: {
+        hometown: ['Austin', 'San Antonio'],
+    },
+    exclude: {
+        age: ['senior'],
+    }
+}
+var clueSix = {
+    type: 'text on phone',
+    description: '(917) 555-1234 - omg bae lol',
+    include: {
+        hometown: 'New York City',
+        age: ['young adult'],
+    },
+    exclude: {
+        gender: 'F'
+    }
+};
+var clueSeven = {
+    type: 'footprints',
+    description: 'Large boots with seeds of a bluebonnet attached to the bottom',
+    include: {
+        profession: ['Engineer', 'Parademic'],
+        hometown: ['Little Rock', 'Orlando', 'LA', 'San Antonio']
+    },
+    exclude: {
+        age: ['young adult']
+    }
+};
+var clueEight = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueNine = {
+    type: 'shirt',
+    description: "logo says 'born in texas' on it ",
+    include: {
+
+    },
+    exclude: {
+        hometown: ["Denver", "LA","New York City", "Little Rock", "Orlando", "Omaha", "Las Vegas", "Seattle"]
+    }
+};
+var ClueTen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueEleven = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueTwelve = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueThirteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueFourteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueFifteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueSixteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueSeventeen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+
+};
+var clueEighteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueNineteen = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
+var clueTwenty = {
+    type: 'drink',
+    description: 'Cosmo in a martini glass',
+    include: {
+        gender: ['F'],
+        age: ['Middle Aged', 'Senior']
+    },
+    exclude: {
+        profession: ['Teacher', 'Salesman']
+    }
+};
