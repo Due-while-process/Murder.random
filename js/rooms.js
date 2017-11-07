bc642974ff642b9994f9dd6d7d7830153bb99327
Elvin <ttop34@icloud.com>
2017-11-07 13:55:57 -0600

    Format all rooms with ID and param

--- js/rooms.js
+++ js/rooms.js
@@ -12,12 +12,75 @@ const LivingRoom = {
         id: 0
     }
 };
-const DiningRoom = {};
-const MasterBedroom = {};
-const Kitchen = {};
-const WineCellar = {};
-const Bathroom = {};
-const HotTub = {};
-const GuestRoom = {};
-const Library = {};
-const Attic = {};
+const DiningRoom = {
+    id: 1,
+    title: 'Dining Room',
+    action: 'investigateRoom',
+    param: {
+        id: 1
+    }
+};
+const MasterBedroom = {
+    id: 2,
+    title: 'Master Bedroom',
+    action: 'investigateRoom',
+    param: {
+        id: 2
+    }
+};
+const Kitchen = {
+    id: 3,
+    title: 'Kitchen',
+    action: 'investigateRoom',
+    param: {
+        id: 3
+    }
+};
+const WineCellar = {
+    id: 4,
+    title: 'Wine Cellar',
+    action: 'investigateRoom',
+    param: {
+        id: 4
+    }
+};
+const Bathroom = {
+    id: 5,
+    title: 'Bathroom',
+    action: 'investigateRoom',
+    param: {
+        id: 5
+    }
+};
+const HotTub = {
+    id: 6,
+    title: 'Kitchen',
+    action: 'investigateRoom',
+    param: {
+        id: 6
+    }
+};
+const GuestRoom = {
+    id: 7,
+    title: 'Guest Room',
+    action: 'investigateRoom',
+    param: {
+        id: 7
+    }
+};
+const Library = {
+    id: 8,
+    title: 'Library',
+    action: 'investigateRoom',
+    param: {
+        id: 8
+    }
+};
+const Attic = {
+    id: 9,
+    title: 'Attic',
+    action: 'investigateRoom',
+    param: {
+        id: 9
+    }
+};
